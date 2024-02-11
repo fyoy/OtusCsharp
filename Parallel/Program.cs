707c87e421a21a8fc71047bb81343dc1117f0ea9
fedorzavaley <fedorzavaley@gmail.com>
2024-02-11 14:57:21 +0300

    ОС: Windows 11 Pro Процессор: 13th Gen Intel(R) Core(TM) i9-13900HX   2.20 GHz
                         |  Последовательно | Паралельно | Паралельно с LINQ
    100000         |                0 ms         |        76 ms     |       34 ms
    1000000       |                4 ms         |        8   ms     |         6 ms
    10000000     |              42 ms         |       79  ms     |       15 ms

---
+++ Parallel/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+int[] sizeList = { 100000, 1000000, 10000000 };
+
+foreach(var size in sizeList)
+{
+    List<int> intList = new();
+
+    for (int i = 0; i < size; i++)
+    {
+        intList.Add(new Random().Next(0, 50));
+    }
+
+    Console.WriteLine("\n");
+    Console.WriteLine($"[{size}]");
+    Console.WriteLine(GetSumm(intList));
+    Console.WriteLine(GetSumParallel(intList));
+    Console.WriteLine(GetSumParallelLINQ(intList));
+}
+
+static string GetSumm(List<int> list)
+{
+    Stopwatch sw = new();
+    int sum = 0;
+
+    sw.Start();
+    foreach(var e in list)
+        sum += e;
+    sw.Stop();
+
+    return $"\t[Sequential] \tSum = {sum}. \tTime = {sw.ElapsedMilliseconds} ms.";
+}
+
+static string GetSumParallel(List<int> list)
+{
+    Stopwatch sw = new();
+    int sum = 0;
+
+    sw.Start();
+    Parallel.For(0, list.Count, () => 0, (i, state, localSum) =>
+    {
+        localSum += list[i];
+        return localSum;
+    },
+    localSum => { Interlocked.Add(ref sum, localSum); });
+    sw.Stop();
+
+    return $"\t[Parallel] \tSum = {sum}. \tTime = {sw.ElapsedMilliseconds} ms.";
+}
+
+static string GetSumParallelLINQ(List<int> list)
+{
+    Stopwatch sw = new();
+
+    sw.Start();
+    var sum = list.AsParallel().Sum();
+    sw.Stop();
+    
+    return $"\t[LINQ Parallel] \tSum = {sum}. \tTime = {sw.ElapsedMilliseconds} ms.";
+}
